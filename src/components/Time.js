2411926e6b16f56c6fd35a71e44b3e33f2490e1e
PauloRev <paulohenriquerev@gmail.com>
2019-10-10 02:37:48 -0300

    correction of comment date

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,16 +1,12 @@
-const Time = ({ timestamp }) => {
-  const date = new Date(timestamp);
-  const hours = date.getHours();
-  const minutes = "0" + date.getMinutes();
-  const seconds = "0" + date.getSeconds();
-
-  const day = "0" + (date.getDay() + 1);
-  const month = "0" + (date.getMonth() + 1);
-  const year = date.getFullYear();
-
-  return `${day.substring(-2)}/${month.substring(
-    -2
-  )}/${year} ${hours}:${minutes.substring(-2)}:${seconds.substring(-2)}`;
-};
+const Time = ({timestamp}) => {
+  const date  = new Date(timestamp)
+  const day   = '0' + date.getDate()
+  const month = '0' + (date.getMonth() + 1)
+  const year  = date.getFullYear()
+  const hour  = '0' + date.getHours()
+  const min   = '0' + date.getMinutes()
+  const sec   = '0' + date.getSeconds()
+  return `${day.substr(-2)}/${month.substr(-2)}/${year} - ${hour.substr(-2)}:${min.substr(-2)}:${sec.substr(-2)}`
+}
 
 export default Time;
